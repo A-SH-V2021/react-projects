edca6d0d87c70769dc8687140deb5a4df749c7e5
unknown <narenje20142014@gmail.com>
2021-07-25 12:55:53 +0430

    compelete prj

--- Navbar/src/Components/Navbar.js
+++ Navbar/src/Components/Navbar.js
@@ -1,23 +1,36 @@
-import React,{useState} from "react";
-import logo from "../logo.svg";
-import { FaBars, FaTwitter } from "react-icons/fa";
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { SiReact } from "react-icons/si";
 import { links, social } from "../Data";
 
 const Navbar = () => {
+  const [value, setValue] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
-  const [value, setValue] = useState(false)
-console.log(value);
+  useEffect(() => {
+    const linksHieght = linksRef.current.clientHeight;
+    if (value) {
+      linksContainerRef.current.style.height = `${linksHieght}px`;
+    } else {
+      linksContainerRef.current.style.height = `0px`;
+    }
+  }, [value]);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} alt="logo" />
-          <button className="nav-toggle" onClick={()=>setValue(!value)}>
+          <div className="logo">
+            <SiReact />
+            <h3 className='logo-title'>react</h3>
+          </div>
+          <button className="nav-toggle" onClick={() => setValue(!value)}>
             <FaBars />
           </button>
         </div>
-        <div className={value ? "links-container show-container" : "links-container"}>
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
