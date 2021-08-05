14c2e96d86dee9d54dabe0eadea68b32d608a0b1
unknown <narenje20142014@gmail.com>
2021-08-06 00:00:51 +0430

    order

--- cocktails/src/Components/Cocktail.js
+++ cocktails/src/Components/Cocktail.js
@@ -11,7 +11,9 @@ const Cocktail = ({ id, name, img, info, glass }) => {
         <h3>{name}</h3>
         <h4>{glass}</h4>
         <p>{info}</p>
-      <Link to={`/cocktails/${id}`} className='btn btn-primary btn-details'>details</Link>
+        <Link to={`/cocktails/${id}`} className="btn btn-primary btn-details">
+          details
+        </Link>
       </div>
     </article>
   );
