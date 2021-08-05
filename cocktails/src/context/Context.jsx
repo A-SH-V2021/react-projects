ea881548fb7ef6dfee8991cbcd2ec3f63e9755cc
unknown <narenje20142014@gmail.com>
2021-08-05 17:21:51 +0430

    compete context.js compete index.css

--- cocktails/src/context/Context.jsx
+++ cocktails/src/context/Context.jsx
@@ -1,12 +1,46 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 export const AppContext = React.createContext();
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 export const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  const [searchItem, setSearchItem] = useState("a");
+  const [loading, setLoading] = useState(false);
+  const [searchItem, setSearchItem] = useState("b");
   const [cocktails, setCocktails] = useState([]);
+
+  const fetchCocktail = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchItem}`);
+      const data = await response.json();
+      const { drinks } = data;
+     
+      if (drinks) {
+        const newItem =   drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+         return {
+            id: idDrink,
+            name: strDrink,
+            img: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newItem)
+      } else {
+        setSearchItem([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCocktail();
+  }, [searchItem]);
 
   return (
     <AppContext.Provider
