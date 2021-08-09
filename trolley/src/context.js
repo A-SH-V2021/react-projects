51f99fffe4ad22661aa408064ac0068388470867
unknown <narenje20142014@gmail.com>
2021-08-09 22:22:45 +0430

    handle clear cart & remove item

--- trolley/src/context.js
+++ trolley/src/context.js
@@ -1,7 +1,8 @@
-import React, {  useContext, useReducer, useEffect } from "react";
+import React, { useState, useContext, useReducer, useEffect } from "react";
 import cartItems from "./data";
 import reducer from "./reducer";
 
+const url = "https://course-api.com/react-useReducer-cart-project";
 const AppContext = React.createContext();
 
 const initialState = {
@@ -12,9 +13,20 @@ const initialState = {
 };
 
 const AppProvider = ({ children }) => {
+  // const [cart, setCart] = useState(cartItems)
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+   return  dispatch({type:'CLEAR_ITEMS'})
+  }
+
+const remove = (id) => {
+  return dispatch({type:'REMOVE',id})
+}
+
+
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state,clearCart,remove }}>{children}</AppContext.Provider>
   );
 };
 // make sure use
--- trolley/src/reducer.js
+++ trolley/src/reducer.js
@@ -1,4 +1,10 @@
-const reducer = (state,action) => {
-    return state
-}
-export default reducer
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_ITEMS") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE") {
+    return { ...state, cart: state.cart.filter((item) => item.id !== action.id) };
+  }
+  return state;
+};
+export default reducer;
