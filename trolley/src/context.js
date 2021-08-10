9eb2defc082b82cbe0b63bc1a1d804ea6047d5a5
unknown <narenje20142014@gmail.com>
2021-08-10 22:32:14 +0430

    compelete calculate

--- trolley/src/context.js
+++ trolley/src/context.js
@@ -12,28 +12,35 @@ const initialState = {
   total: 0,
 };
 
+
 const AppProvider = ({ children }) => {
   // const [cart, setCart] = useState(cartItems)
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const clearCart = () => {
-   return  dispatch({type:'CLEAR_ITEMS'})
-  }
+    return dispatch({ type: "CLEAR_ITEMS" });
+  };
 
-const remove = (id) => {
-  return dispatch({type:'REMOVE',id})
-}
-const increase = (id) => {
-  return dispatch({type:'INCREASE',id})
-}
-const decrease = (id) => {
-  return dispatch({type:'DECREASE',id})
-}
-
-
+  const remove = (id) => {
+    return dispatch({ type: "REMOVE", id });
+  };
+  const increase = (id) => {
+    return dispatch({ type: "INCREASE", id });
+  };
+  const decrease = (id) => {
+    return dispatch({ type: "DECREASE", id });
+  };
+  
+  useEffect(() => {
+     dispatch({type:'GET_TOTAL'})
+  }, [state.cart]);
 
   return (
-    <AppContext.Provider value={{ ...state,clearCart,remove,increase,decrease}}>{children}</AppContext.Provider>
+    <AppContext.Provider
+      value={{ ...state, clearCart, remove, increase, decrease }}
+    >
+      {children}
+    </AppContext.Provider>
   );
 };
 // make sure use
--- trolley/src/reducer.js
+++ trolley/src/reducer.js
@@ -2,12 +2,14 @@ const reducer = (state, action) => {
   if (action.type === "CLEAR_ITEMS") {
     return { ...state, cart: [] };
   }
+
   if (action.type === "REMOVE") {
     return {
       ...state,
       cart: state.cart.filter((item) => item.id !== action.id),
     };
   }
+
   if (action.type === "INCREASE") {
     const newCart = state.cart.map((item) => {
       if (item.id === action.id) {
@@ -20,17 +22,37 @@ const reducer = (state, action) => {
   }
 
   if (action.type === "DECREASE") {
-   
-    const newCart = state.cart.map((item) => {
-    
-      if (item.id === action.id ) {
-        return { ...item, amount: item.amount - 1 };
-      } else {
-        return item;
+    const newCart = state.cart
+      .map((item) => {
+        if (item.id === action.id) {
+          return { ...item, amount: item.amount - 1 };
+        } else {
+          return item;
+        }
+      })
+      .filter((item) => item.amount !== 0);
+    return { ...state, cart: newCart };
+  }
+
+  if (action.type === "GET_TOTAL") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
       }
-    }).filter((item)=>item.amount!==0)
-return {...state,cart:newCart}
-  } 
+    );
+    total = parseFloat(total.toFixed(2));
+    return { ...state, total, amount };
+  }
   return state;
 };
+
 export default reducer;
