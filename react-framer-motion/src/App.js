f18bc2bbcf30e7f40310f464334364ad748d3944
Alchemist <60227585+A-SH-V2021@users.noreply.github.com>
2021-10-01 05:58:15 +0330

    Add files via upload

--- react-framer-motion/src/App.js
+++ react-framer-motion/src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
 import Home from "./Pages/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import About from "./Pages/About";
 import Services from "./Pages/Services";
-
+import GlobalStyle from "./GlobalStyle";
+import { AnimatePresence } from "framer-motion";
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/services" component={Services} />
-      </Switch>
-    </Router>
+    <>
+      <GlobalStyle />
+      <AnimatePresence exitBeforeEnter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+        </Switch>
+      </AnimatePresence>
+    </>
   );
 }
 
